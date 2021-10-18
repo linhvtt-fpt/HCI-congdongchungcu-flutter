aff22d5e48b7ffa4ccef1aaa85e7354d911e8f2c
lichsucauthu <anhnguyentien3006@gmail.com>
2021-10-18 23:39:05 +0700

    tạo trang chi tiết cho đơn xác nhận và cả chưa xác nhận của bên Customer và Shop

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -29,146 +29,146 @@ class _ProfilePageState extends State<ProfilePage> {
     List textItems = ["Chờ xác nhận", "Đã xác nhận", "Đánh giá"];
 
     return Scaffold(
-        // appBar: buildAppBar(context),
+      // appBar: buildAppBar(context),
 
-      body: ListView(
-      physics: BouncingScrollPhysics(),
-      children: [
-        SizedBox(
-          height: 150, 
-          child: Column(
-            children: [
-              SizedBox(height: 10,),
-        ProfileWidget(
-          imagePath: user.imagePath,
-          //hieu ung mo
-          onClicked: () async {
-            Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => EditProfilePage()),
-            );
-          },
-        ),
-                
-        SizedBox(height: 10),
-        buildName(user),
-            ],
-          ),
-        ),
-
-        SizedBox(height: 30),
-        Column(
+        body: ListView(
+          physics: BouncingScrollPhysics(),
           children: [
-            Row(
+            SizedBox(
+              height: 150,
+              child: Column(
+                children: [
+                  SizedBox(height: 10,),
+                  ProfileWidget(
+                    imagePath: user.imagePath,
+                    //hieu ung mo
+                    onClicked: () async {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => EditProfilePage()),
+                      );
+                    },
+                  ),
+
+                  SizedBox(height: 10),
+                  buildName(user),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 30),
+            Column(
               children: [
-                SizedBox(width: 20,),
-                Icon(Icons.list_alt, color: Colors.blue[900],size: 30,),
-                SizedBox(width: 10,),
-                Text("Đơn mua", style: TextStyle( fontSize: 18),)
+                Row(
+                  children: [
+                    SizedBox(width: 20,),
+                    Icon(Icons.list_alt, color: Colors.blue[900],size: 30,),
+                    SizedBox(width: 10,),
+                    Text("Đơn mua", style: TextStyle( fontSize: 18),)
+                  ],
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      color: white,
+                      border: Border(
+                          top: BorderSide(width: 2, color: black.withOpacity(0.06)),
+                          bottom: BorderSide(width: 2, color: black.withOpacity(0.06)))
+                  ),
+                  child: Padding(
+                    padding:
+                    const EdgeInsets.only(left: 30, right: 30, bottom: 0, top: 10),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: List.generate(textItems.length, (index) {
+                        return InkWell(
+                            onTap: () {
+                              selectedTab(index);
+                            },
+                            child: Column(
+                              children: [
+                                Icon(
+                                    bottomItems[index],
+                                    size: 22,
+                                    color: Colors.black
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  textItems[index],
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      color: black),
+                                ),
+                              ],
+                            ));
+                      }),
+                    ),
+                  ),
+                ),
               ],
             ),
-            Container(
-      width: double.infinity,
-      height: 60,
-      decoration: BoxDecoration(
-              color: white,
-              border: Border(
-                  top: BorderSide(width: 2, color: black.withOpacity(0.06)),
-                  bottom: BorderSide(width: 2, color: black.withOpacity(0.06)))
-                  ),
-      child: Padding(
-            padding:
-                const EdgeInsets.only(left: 30, right: 30, bottom: 0, top: 10),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: List.generate(textItems.length, (index) {
-                return InkWell(
-                    onTap: () {
-                      selectedTab(index);
-                    },
-                    child: Column(
-                      children: [
-                        Icon(
-                          bottomItems[index],
-                          size: 22,
-                          color: Colors.black
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text(
-                          textItems[index],
-                          style: TextStyle(
-                              fontSize: 15,
-                              color: black),
-                        ),
-                      ],
-                    ));
-              }),
+            SizedBox(height: 30),
+            Row(
+              children: [
+                _heading("Thông tin cá nhân"),
+                SizedBox(
+                    width: 150,
+                    height: 30,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => MyShopPage()),
+                        );
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.red),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8))),
+                      ),
+                      child: Row(
+                        children: [
+                          Icon(Icons.storefront, size: 20,),
+                          SizedBox(width: 10,),
+                          Text("Shop của tôi")
+                        ],
+                      ),
+                    )
+                )
+              ],
             ),
-      ),
-    ),
-          ],
-        ),
-        SizedBox(height: 30),
-        Row(
-          children: [
-            _heading("Thông tin cá nhân"),
+
             SizedBox(
-              width: 150,
-              height: 30,
-              child: ElevatedButton(
-                  onPressed: () {
-                   Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => MyShopPage()),
-            );
-                  },
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.red),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8))),
-                  ),
-                  child: Row(
-                    children: [
-                      Icon(Icons.storefront, size: 20,),
-                      SizedBox(width: 10,),
-                      Text("Shop của tôi")
-                    ],
-                  ),
-              )
-            )
+              height: 5,
+            ),
+            _profileDetail(),
+            SizedBox(
+              height: 5,
+            ),
+            _heading("Cài đặt"),
+            SizedBox(
+              height: 5,
+            ),
+            _settingCard(),
+            SizedBox(
+              height: 5,
+            ),
+            _btnLogout(),
           ],
-        ),
-        
-        SizedBox(
-          height: 5,
-        ),
-        _profileDetail(),
-        SizedBox(
-          height: 5,
-        ),
-        _heading("Cài đặt"),
-        SizedBox(
-          height: 5,
-        ),
-        _settingCard(),
-        SizedBox(
-          height: 5,
-        ),
-        _btnLogout(),
-      ],
-    ));
+        ));
   }
 
   Widget buildName(User user) => Column(
-        children: [
-          Text(
-            user.name,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
-          )
-        ],
-      );
+    children: [
+      Text(
+        user.name,
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+      )
+    ],
+  );
 
   Widget _heading(String heading) {
     return Container(
@@ -192,12 +192,12 @@ class _ProfilePageState extends State<ProfilePage> {
               Row(children: [
                 Padding(
                     padding: EdgeInsets.only(left: 20, bottom: 10, top: 45)),
-                    Icon(Icons.alternate_email),
-                    SizedBox(width: 30,),
-                    Text(
-                      "erenYeager@gmail.com",
-                      style: TextStyle(fontSize: 16),
-                    )
+                Icon(Icons.alternate_email),
+                SizedBox(width: 30,),
+                Text(
+                  "erenYeager@gmail.com",
+                  style: TextStyle(fontSize: 16),
+                )
               ]),
               Divider(
                 height: 0.6,
@@ -206,12 +206,12 @@ class _ProfilePageState extends State<ProfilePage> {
               Row(children: [
                 Padding(
                     padding: EdgeInsets.only(left: 20, bottom: 10, top: 45)),
-                    Icon(Icons.phone),
-                    SizedBox(width: 30,),
-                    Text(
-                      "09236738253",
-                      style: TextStyle(fontSize: 16),
-                    )
+                Icon(Icons.phone),
+                SizedBox(width: 30,),
+                Text(
+                  "09236738253",
+                  style: TextStyle(fontSize: 16),
+                )
               ]),
               Divider(
                 height: 0.6,
@@ -220,12 +220,12 @@ class _ProfilePageState extends State<ProfilePage> {
               Row(children: [
                 Padding(
                     padding: EdgeInsets.only(left: 20, bottom: 10, top: 45)),
-                    Icon(Icons.add_location_alt_rounded),
-                    SizedBox(width: 30,),
-                    Text(
-                      "Topaz Home apartment, Block 3",
-                      style: TextStyle(fontSize: 16),
-                    )
+                Icon(Icons.add_location_alt_rounded),
+                SizedBox(width: 30,),
+                Text(
+                  "Topaz Home apartment, Block 3",
+                  style: TextStyle(fontSize: 16),
+                )
               ]),
             ],
           ),
@@ -305,7 +305,7 @@ class _ProfilePageState extends State<ProfilePage> {
         ));
   }
 
-  
+
   selectedTab(index) {
     setState(() {
       pageIndex = index;
