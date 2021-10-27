e1c8384b4a370c4f71e31af73f3dbe20652d93dd
linhvtt-fpt <linhvu263@gmail.com>
2021-10-28 00:40:23 +0700

    commit

---
+++ lib/json/khuyen_mai_json.dart
@@ -0,0 +1,97 @@
+import 'khuyen_mai_model.dart';
+import 'product_model.dart';
+
+List<Sale> listSale = [
+  Sale(
+      listProduct: [
+        Product(
+            urlImage:
+                "https://papishop.vn/wp-content/uploads/2020/07/TR401riMsee1tk6nThkXJA_355305939-600x600.jpg",
+            name: "Balo hồng dễ thương",
+            description: "Balo hồng chống nước, đựng vừa laptop 15inch",
+            price: 130000,
+            imgShop: "assets/images/avtAnVat.jpg",
+            nameShop: "Chuyên sỉ balo",
+            rate: "4.8",
+            rate_number: "90",
+            quantity: 1,
+            categoryId: 1,
+            isSale: true,
+            saleNum: 10),
+        Product(
+            name: "Nui chiên giòn túi 100g",
+            description:
+                "Vừa ăn vừa xem phim, đọc sách rất phù hợp hoặc thưởng thức trà.",
+            price: 35000,
+            urlImage:
+                'https://afamilycdn.com/150157425591193600/2020/4/1/9115720610044658732819171265064230814102841n-15857137102061364901955.jpg?fbclid=IwAR1sKi1NbeZc2lIiAgA2l9UycrXwELij59Z7TW3QZXQ7D5Nodn8IUCkN3Xk',
+            imgShop: "assets/images/avtAnVat.jpg",
+            nameShop: "Ăn vặt ngon",
+            rate: "4.9",
+            rate_number: "100",
+            quantity: 1,
+            categoryId: 1,
+            isSale: true,
+            saleNum: 20),
+        Product(
+            urlImage:
+                "https://cf.shopee.vn/file/09e9dea5d18e1a4fd554b7c61fffe29b?fbclid=IwAR0PWICjTQmHNIYJ61FOoSPjrU1KKD9kfELzyh5zBb8Bu_ofq2S06mXNLEo",
+            name: "Combo bánh kẹo Thái Lan",
+            description: "Combo bánh kẹo Thái Lan siêu ngon",
+            price: 180000,
+            imgShop: "assets/images/avtAnVat.jpg",
+            nameShop: "Ăn ngon ngon",
+            rate: "4.9",
+            rate_number: "110",
+            quantity: 1,
+            categoryId: 1,
+            isSale: true,
+            saleNum: 30),
+        Product(
+            urlImage:
+                "https://salt.tikicdn.com/ts/tmp/d3/2c/0c/c0c4e89bfbf23efe3b18d9b10883a7ee.jpg",
+            name: "Váy công chúa bé gái",
+            description: "váy công chúa dành cho bé gái từ 7 - 10 tuổi",
+            price: 250000,
+            imgShop: "assets/images/avtAnVat.jpg",
+            nameShop: "Kid Store",
+            rate: "4.8",
+            rate_number: "80",
+            quantity: 1,
+            categoryId: 1,
+            isSale: true,
+            saleNum: 30),
+        // Product(
+        //     urlImage:
+        //         "https://lh3.googleusercontent.com/proxy/a9HkqYPM0AsHzkLVp7nBelhMme3EuFRle3bqmRcfviVl-EYB5uSmLyROj4xbZ8KEKCWgINd8pTfVRcEE7s0UEq1tQvL2MkPd717Bqa-HR-6GkVbILau7gbj4UvhEG8FrWEJ0ybl2ZbKA",
+        //     name: "Bánh tráng trộn",
+        //     description: "Bánh tráng trộn nhà làm ngon đảm bảo an toàn",
+        //     price: 20000,
+        //     imgShop: "assets/images/avtAnVat.jpg",
+        //     nameShop: "Ăn vặt ngon",
+        //     rate: "4.3",
+        //     rate_number: "50",
+        //     quantity: 1,
+        //     categoryId: 1,
+        //     isSale: true,
+        //     saleNum: 30),
+        // Product(
+        //     urlImage:
+        //         "https://f19-zpg.zdn.vn/1774453824866563078/56d469136e649e3ac775.jpg?fbclid=IwAR2rymCV1bWOvD1vDtB-u8zXsqlhEEvVOOyVk-7tBZlar-FAyUB9Ppx9SFU",
+        //     name: "Áo nỉ tay dài màu trắng",
+        //     description: "Áo nỉ tay dài màu trắng chất vải siêu đẹp",
+        //     price: 210000,
+        //     imgShop: "assets/images/avtAnVat.jpg",
+        //     nameShop: "Women Fashion",
+        //     rate: "4.7",
+        //     rate_number: "70",
+        //     quantity: 1,
+        //     categoryId: 1,
+        //     isSale: true,
+        //     saleNum: 30),
+      ],
+      start_date_time: "28-10-2021 10:00",
+      end_date_time: "28-10-2021 18:00",
+      name: "Khuyến mãi đồ ăn vặt",
+      saleNum: 20)
+];
---
+++ lib/pages/chuong_trinh_khuyen_mai_page.dart
@@ -0,0 +1,183 @@
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:test_flutter_template/json/khuyen_mai_json.dart';
+import 'package:test_flutter_template/json/khuyen_mai_model.dart';
+import 'package:test_flutter_template/theme/colors.dart';
+
+class ChuongTrinhKhuyenMaiPage extends StatefulWidget {
+  const ChuongTrinhKhuyenMaiPage({Key? key}) : super(key: key);
+
+  @override
+  _ChuongTrinhKhuyenMaiPageState createState() =>
+      _ChuongTrinhKhuyenMaiPageState();
+}
+
+class _ChuongTrinhKhuyenMaiPageState extends State<ChuongTrinhKhuyenMaiPage> {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(title: Text("Chương trình khuyến mãi")),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(8),
+        child: Column(
+          children: List.generate(listSale.length, (index) {
+            return Container(
+              padding: EdgeInsets.all(20),
+              width: 400,
+              // height: 150,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                ),
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    offset: const Offset(
+                      5.0,
+                      5.0,
+                    ),
+                    blurRadius: 10.0,
+                    spreadRadius: 2.0,
+                  ), //BoxShadow
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: const Offset(0.0, 0.0),
+                    blurRadius: 0.0,
+                    spreadRadius: 0.0,
+                  ), //BoxShadow
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "Tên chương trình: ",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 17),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        listSale[index].name,
+                        style: TextStyle(fontSize: 17),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Text("Thời gian bắt đầu: ",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 17)),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(listSale[index].start_date_time,
+                          style: TextStyle(fontSize: 17))
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Text("Thời gian kết thúc: ",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 17)),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(listSale[index].end_date_time,
+                          style: TextStyle(fontSize: 17))
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Text("Giảm: ",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 17)),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(listSale[index].saleNum.toString() + "%",
+                          style: TextStyle(fontSize: 17))
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: List.generate(
+                          listSale[index].listProduct.length, (index1) {
+                        return Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Image.network(
+                              listSale[index].listProduct[index1].urlImage,
+                              width: 80,
+                              height: 80,
+                            ));
+                      })),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          fixedSize: const Size(100, 30),
+                          primary: Colors.red[200], // background
+                          onPrimary: Colors.black, // foreground
+                        ),
+                        onPressed: () {},
+                        child: const Text('Thay đổi'),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                           fixedSize: const Size(100, 30),
+                          primary: Colors.red[200], // background
+                          onPrimary: Colors.black, // foreground
+                        ),
+                        onPressed: () {},
+                        child: const Text('Xóa'),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            );
+          }),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        width: 100,
+        height: 50,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              textStyle:
+                  const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              onPrimary: Colors.white,
+              primary: Colors.red),
+          onPressed: () {},
+          child: const Text('Tạo chương trình khuyến mãi'),
+        ),
+      ),
+    );
+  }
+}
